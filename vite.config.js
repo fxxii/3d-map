1bcd242310b689f403e3b471f7b04a0284fac7a2
gram <gram@fxxii.com>
2026-01-16 12:07:10 +0000

    chore: Add Vite config and GitHub Action for auto-deployment

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+export default {
+  base: './', // Ensures assets are linked relatively (e.g. "assets/script.js" instead of "/assets/script.js")
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
+  }
+}
